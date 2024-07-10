dd09e8ceee124bd2b66e103b387ff6ae8330fc21
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-07-10 15:40:56 +0530

    Update SpiralMatrix.java

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -5,57 +5,47 @@ class Solution {
         int row = l;
         int column = matrix[0].length;
         int i=0,j=0;
-        int prevrow = -1;
-        int prevcolumn = -1;
-        int flag=0;
-        while(row>prevrow && column>prevcolumn){
-            for(j=j;j<column;j++){
-                flag=1;
-                if(j>prevcolumn)
-                  a.add(matrix[i][j]);
+        int prevrow = 0;
+        int prevcolumn = 0;
+        int flag=(matrix.length*matrix[0].length);
+        while(l>0){
+            System.out.println(a.size());
+            if(a.size()==flag)
+                break;
+
+            for(j=prevcolumn;j<column;j++){
+                a.add(matrix[i][j]);
             }
-    
-            if(flag==1){
-                j = j-1;
-                prevrow = prevrow+1;
-            }
+            j = j-1;
+            prevrow = prevrow+1;
             
-            flag=0;
-            for(i=i;i<row;i++){
-                flag=1;
-                if(i>prevrow )
-                    a.add(matrix[i][j]);
+            if(a.size()==flag)
+                break;
+            for(i=prevrow;i<row;i++){
+                a.add(matrix[i][j]);
             }
-            if(flag==1){
-                i=i-1;
-                
-            }
+            i=i-1;
             column--;
-            flag=0;
-            for(j=j;j>prevcolumn;j--){
-                flag=1;
-                if(j<column)
-                    a.add(matrix[i][j]);
-            }
-            if(flag==1){
-                row--;
             
-            }
+            if(a.size()==flag)
+                break;
+            for(j=column-1;j>=prevcolumn;j--){
+                a.add(matrix[i][j]);
+            } 
+            row--;
             j=j+1;
-            flag=0;
-            for(i=i;i>prevrow;i--){
-                flag=1;
-                if(i<row)
-                    a.add(matrix[i][j]);
-            }
-            if(flag==1){
-                i=i+1;
             
+            if(a.size()==flag)
+                break;
+            for(i=row-1;i>=prevrow;i--){
+                a.add(matrix[i][j]);
             }
+            i=i+1;
             prevcolumn++;
-            flag=0;
+
             l=l-2;
         }
+        System.out.println(a.size());
         return a;
     }
 }
