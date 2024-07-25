6149e789704f3be4f299f5cd777347919adffe3f
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-07-25 07:17:38 +0530

    [39] [Time Beats: 99.90%] [Memory Beats: 6.22%] - LeetPush

---
+++ 39-Combination-Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void combination(int i,int target,int n,int []arr,ArrayList<Integer> p,
+    List<List<Integer>> res){
+        if(i==n){
+            if(target==0){
+                res.add(new ArrayList<>(p));
+                return;
+            }
+            else    return;
+        }
+        if(arr[i]<=target){
+            p.add(arr[i]);
+            combination(i,target-arr[i],n,arr,p,res);
+            p.remove(p.size()-1);
+        }
+        combination(i+1,target,n,arr,p,res);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        combination(0,target,candidates.length,candidates,new ArrayList<Integer>(),res);
+        return res;
+    }
+}
