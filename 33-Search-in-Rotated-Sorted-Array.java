6a8b84687c43871d428d5a65e1f7c8c706e81f93
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-07-30 21:48:12 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 5.83%] - LeetPush

---
+++ 33-Search-in-Rotated-Sorted-Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int search(int[] arr, int k) {
+        if(arr.length==1){
+            return arr[0]==k?0:-1;
+        }
+        int i = 0;
+        int j = arr.length - 1;
+        int mid = i + (j - i) / 2;
+        while (i <= j) {
+            mid = i + (j - i) / 2;
+            if (arr[mid] == k) {
+                return mid;
+            }
+            if (arr[i]<=arr[mid]) {
+                if (arr[i]<= k && arr[mid]>=k)
+                    j = mid-1;
+                else
+                    i=mid+1;
+            } else {
+                if(arr[mid]<=k && arr[j]>=k)
+                    i=mid+1;
+                else
+                    j = mid-1;
+            }
+        }
+        return -1;
+    }
+}
