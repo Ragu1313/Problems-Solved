63ec19ebb4c63c4b1355fa93a8d4193578a59b27
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-07-30 20:22:29 +0530

    [162] [Time Beats: 100.00%] [Memory Beats: 33.79%] - LeetPush

--- 162-Find-Peak-Element.java
+++ 162-Find-Peak-Element.java
@@ -1,15 +1,5 @@
 class Solution {
-
-    
-    public int findPeakElement(int[] arr) {
-        int n = arr.length;
-        if(n==0 || n==1)
-            return 0;
-        if(arr[n-1]>arr[n-2])
-            return n-1;
-        if(arr[0]>arr[1])
-            return 0;
-        int i=1,j = n-1;
+    public int binary(int i,int j,int []arr){
          while(i<=j){
             int mid = i+(j-i)/2;
             if(arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1])
@@ -21,6 +11,16 @@ class Solution {
                 j = mid-1;
         }
         return -1;
-        // return binary(0,nums.length-1,nums);
+    }
+    
+    public int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if(n==0 || n==1)
+            return 0;
+        if(arr[n-1]>arr[n-2])
+            return n-1;
+        if(arr[0]>arr[1])
+            return 0;
+        return binary(1,n-1,arr);
     }
 }
