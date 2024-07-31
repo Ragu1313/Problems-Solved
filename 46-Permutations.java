8a91e40ba90b6f8e03cb8bbf24edc8f55fd69d56
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-07-31 14:09:20 +0530

    [46] [Time Beats: 94.09%] [Memory Beats: 75.90%] - LeetPush

--- 46-Permutations.java
+++ 46-Permutations.java
@@ -1,27 +1,25 @@
 class Solution {
-    public void per(int n,int arr[],List<Integer> p,List<List<Integer>> res,boolean flag[]){
+    public void per(int n,int nums[],ArrayList<Integer> p,
+    List<List<Integer>> res,boolean flag[] ){
         if(p.size()==n){
             res.add(new ArrayList<>(p));
             return ;
         }
-        for(int j=0;j<=n-1;j++){
-            if(!flag[j]){
-                flag[j] = true;
-                p.add(arr[j]);
-                per(n,arr,p,res,flag);
+        for(int i=0;i<n;i++){
+            if(!flag[i]){
+                p.add(nums[i]);
+                flag[i]=true;
+                per(n,nums,p,res,flag);
+                flag[i] = false;
                 p.remove(p.size()-1);
-                flag[j] = false;
             }
-            // per(n,arr,p,res,flag);
         }
-
     }
     public List<List<Integer>> permute(int[] nums) {
         int n = nums.length;
         boolean [] flag = new boolean[n];
         List<List<Integer>> a = new ArrayList<>();
-        // for(int i=0;i<n;i++)
-            per(n,nums,new ArrayList<>(),a,flag);
+        per(n,nums,new ArrayList<>(),a,flag);
         return a;
     }
 }
