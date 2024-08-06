060dc5884de81dc35937b7438ffb5a60546a7a86
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-06 23:29:40 +0530

    [3016] [Time Beats: 91.92%] [Memory Beats: 79.42%] - LeetPush

---
+++ 3016-Minimum-Number-of-Pushes-to-Type-Word-II.java
@@ -0,0 +1,40 @@
+class Solution {
+     public static void quicksort(int low,int high,int end[]){
+        if(low>=high)
+			 return ;
+        int i = low;
+        int j = high;
+        int mid = i+(j-i)/2;
+        int pivot = end[mid];
+        while(i<=j){
+            while(end[i]>pivot)
+            i++;
+            while(end[j]<pivot)
+            j--;
+            if(i<=j){
+                int temp = end[i];
+                end[i] = end[j];
+                end[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        quicksort(low,j,end);
+        quicksort(i,high,end);
+    }
+    public int minimumPushes(String word) {
+        // Map<Character,count>
+        int arr[] = new int [26];
+        for(char ch : word.toCharArray()){
+            arr[ch-'a']++;
+        }
+        quicksort(0,25,arr);
+        int total = 0;
+        for(int i=0;i<26;i++){
+            if(arr[i]==0) break;
+            int temp = i/8 +1;
+            total = total + (temp * arr[i]);
+        }
+        return total;
+    }
+}
