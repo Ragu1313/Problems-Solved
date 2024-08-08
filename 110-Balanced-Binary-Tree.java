2d09614e1b2fc78d1be542fd6e4a66600e68ca99
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-08 18:13:09 +0530

    [110] [Time Beats: 100.00%] [Memory Beats: 38.08%] - LeetPush

--- 110-Balanced-Binary-Tree.java
+++ 110-Balanced-Binary-Tree.java
@@ -14,23 +14,20 @@
  * }
  */
 class Solution {
-    public int inorder(TreeNode root){
-        if(root==null){
+    int check(TreeNode root){
+        if(root==null)
             return 0;
-        }
-            int left = inorder(root.left);
-            if(left==Integer.MIN_VALUE)
-                return left;
-            int right = inorder(root.right);
-            if(right==Integer.MIN_VALUE)
-                return right;
-            if(Math.abs(left-right)>1)
-                return Integer.MIN_VALUE;
-            return Math.max(left,right)+1;
+        int left = check(root.left);
+        int right = check(root.right);
+        if(Math.abs(left-right)>1)
+            return -1;
+        if(left==-1|| right==-1)
+            return -1;
+        return Math.max(left,right)+1;
     }
     public boolean isBalanced(TreeNode root) {
-        int res = inorder(root);
-        if(res==Integer.MIN_VALUE ){
+        int res = check(root);
+        if(res==-1 ){
             return false;
         }
         return true;
