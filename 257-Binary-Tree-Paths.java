c8fdfb71b468a33ccc6116fa43814f4531f1fc67
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-10 21:13:10 +0530

    [257] [Time Beats: 40.95%] [Memory Beats: 11.60%] - LeetPush

--- 257-Binary-Tree-Paths.java
+++ 257-Binary-Tree-Paths.java
@@ -15,29 +15,29 @@
  */
 class Solution {
     List<String> res = new ArrayList<>();
-    public boolean isleaf(TreeNode a){
-        if(a.left==null && a.right==null)
-            return true;
-        return false;
-    }
-    public void inorder(TreeNode root,String s){
-        if(root==null){
-            return ;
+    
+    public void dfs(TreeNode node, String path) {
+        if (node == null) {
+            return;
         }
-        if(isleaf(root))
-            res.add(s+\->\+String.valueOf(root.val));
-        inorder(root.left,s+\->\+String.valueOf(root.val));
-        // if(isleaf(root))
-        //     res.add(s+\->\+String.valueOf(root.val));
-        inorder(root.right,s+\->\+String.valueOf(root.val));
+        
+        // Append the current node's value to the path
+        path += (path.isEmpty() ? \\ : \->\) + node.val;
+        
+        // If it's a leaf node, add the path to the result
+        if (node.left == null && node.right == null) {
+            res.add(path);
+        } else {
+            // Continue to traverse the left and right subtree
+            dfs(node.left, path);
+            dfs(node.right, path);
+        }
     }
+    
     public List<String> binaryTreePaths(TreeNode root) {
-        if(root.left==null && root.right==null){
-            res.add(String.valueOf(root.val));
-            return res;
+        if (root != null) {
+            dfs(root, \\);
         }
-        inorder(root.left,String.valueOf(root.val));
-        inorder(root.right,String.valueOf(root.val));
         return res;
     }
 }
