b4c1dc3eccf7acc4e93f1c521907e9d71284e335
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-19 14:59:38 +0530

    [322] [Time Beats: 15.37%] [Memory Beats: 15.14%] - LeetPush

---
+++ 322-Coin-Change.java
@@ -0,0 +1,32 @@
+class Solution {
+     public static int minimum(int i,int target,int arr[],int dp[][]){
+        if(i==0){
+            if(target%arr[i]==0)
+                return target/arr[i];
+            else
+                return (int)Math.pow(10,9);
+        }
+        if(target==0)
+            return 0;
+        if(dp[i][target]!=-1)
+            return dp[i][target];
+        int take = Integer.MAX_VALUE;
+        int nottake =0+ minimum(i-1,target,arr,dp);
+        if(arr[i]<=target)
+            take = 1+ minimum(i,target-arr[i],arr,dp);
+        return dp[i][target] = Math.min(take,nottake);        
+    }
+    public static int coinChange(int num[], int x) {
+        // Write your code here..
+        int [][] dp = new int[num.length][x+1];
+        for(int i[] : dp)
+            Arrays.fill(i,-1);
+        int res = minimum(num.length-1,x,num,dp);
+        if (res >= (int) Math.pow(10, 9))
+            return -1;
+        return res;
+    }
+    // public int coinChange(int[] coins, int amount) {
+        
+    // }
+}
