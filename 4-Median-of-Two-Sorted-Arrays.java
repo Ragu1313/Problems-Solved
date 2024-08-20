235f35095635fd0dd50408edba7d1a658edae1b6
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-20 14:49:46 +0530

    [4] [Time Beats: 100.00%] [Memory Beats: 36.87%] - LeetPush

--- 4-Median-of-Two-Sorted-Arrays.java
+++ 4-Median-of-Two-Sorted-Arrays.java
@@ -1,25 +1,67 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length ;
-        int n = nums2.length;
-        int a[] = new int[m+n];
-        int l = m+n;
-        int c=0;
-        float result=0;int i,j;
-        for(i=0;i<m;i++){
-            a[i]=nums1[i];
+    public double findMedianSortedArrays(int[] arr1, int[] arr2) {
+        int p1 = 0;
+        int p2 = 0;
+        int res1 = 0;
+        int res2 = 0;
+        int mid = (arr1.length+arr2.length)/2+1;
+        // System.out.println(mid);
+        if(mid==1){
+            if(arr1.length==1)
+                return (float) arr1[0];
+            else
+                return (float) arr2[0];
         }
-        for(j=m;j<m+n;j++){
-            a[j] = nums2[c++];
+        if(arr2.length==0){
+            mid = arr1.length/2;
+            if(arr1.length%2==0){
+                
+                return (float)(arr1[mid-1]+arr1[mid])/2;
+            }
+            else
+                return (float)arr1[mid];
         }
-        Arrays.sort(a);
-        if(l%2==0){
-          result = (float)(a[l/2]+a[(l-1)/2])/2;
+        if(arr1.length==0){
+            mid = arr2.length/2;
+            if(arr2.length%2==0){
+                
+                return (float)(arr2[mid-1]+arr2[mid])/2;
+            }
+            else
+                return (float)arr2[mid];
         }
-        else{
-            result = a[(l)/2];
+        while(mid-->0){
+            if(p1 < arr1.length && arr1[p1]<=arr2[p2] ){
+                res1 = res2;
+                res2 = arr1[p1];
+                p1++;
+            }
+            else if(p2 < arr2.length){
+                res1 = res2;
+                res2 = arr2[p2];
+                p2++;
+            }
+            if(p1>=arr1.length || p2>=arr2.length)
+                break;
         }
-          
-        return result; 
+        if(mid!=0 ){
+            if( p1<arr1.length){
+                while(mid-->0){
+                    res1 = res2;
+                    res2 = arr1[p1];
+                    p1++;
+                }
+            }
+            else{
+                while(mid-->0){
+                    res1 = res2;
+                    res2 = arr2[p2];
+                    p2++;
+                }
+            }
+        }
+        if((arr1.length+arr2.length)%2==0)
+        return (float)(res1+res2)/2;
+        return (float)res2;
     }
 }
