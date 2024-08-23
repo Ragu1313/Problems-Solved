04521470b6046e8a0856efda88a0d74fa793df28
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-23 21:18:12 +0530

    [123] [Time Beats: 48.91%] [Memory Beats: 43.11%] - LeetPush

--- 123-Best-Time-to-Buy-and-Sell-Stock-III.java
+++ 123-Best-Time-to-Buy-and-Sell-Stock-III.java
@@ -7,20 +7,26 @@ class Solution {
         if(dp[i][flag][count]!=-1)
             return dp[i][flag][count];
         int m = 0;
-        if(flag==0){
+        if(flag==0)
             m = Math.max(max1(i+1,1,count,n,arr,dp)-arr[i],max1(i+1,0,count,n,arr,dp));
-        }
         else
             m = Math.max(max1(i+1,0,count+1,n,arr,dp)+arr[i],max1(i+1,1,count,n,arr,dp));
         return dp[i][flag][count] = m;
     }
     public int maxProfit(int[] arr) {
-        int dp [][][] = new int [arr.length][2][2];
-        for(int i[][]:dp){
-            for(int j[]:i){
-                Arrays.fill(j,-1);
+        int dp [][][] = new int [arr.length+1][2][3];
+        int m;
+        for(int i=arr.length-1;i>=0;i--){
+            for(int flag=0;flag<2;flag++){
+                for(int count=0;count<2;count++){
+                    if(flag==0)
+                        m = Math.max(dp[i+1][1][count]-arr[i],dp[i+1][0][count]);
+                    else
+                        m = Math.max(dp[i+1][0][count+1]+arr[i],dp[i+1][1][count]);
+                    dp[i][flag][count] = m;
+                }
             }
         }
-        return max1(0,0,0,arr.length-1,arr,dp);
+        return dp[0][0][0];
     }
 }
