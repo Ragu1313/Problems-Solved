1d46102fca0612c01c94a309ccd2f7c64a53dc94
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-08-25 05:36:42 +0530

    [309] [Time Beats: 100.00%] [Memory Beats: 33.11%] - LeetPush

--- 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.java
+++ 309-Best-Time-to-Buy-and-Sell-Stock-with-Cooldown.java
@@ -10,22 +10,22 @@ class Solution {
         return dp[i][flag] = m;
     }
     public int maxProfit(int[] arr) {
-        int dp [][] = new int [arr.length+2][2];
-        // for(int i[]:dp){
-            // for(int j[]:i){
-                // Arrays.fill(i,-1);
-            // }
-        // }
+        // int dp [][] = new int [arr.length+2][2];
+        int next[] = new int[2];
+        int cur[] = new int[2];
+        int nextnext[] = new int[2]; 
         for(int i=arr.length-1;i>=0;i--){
             int m=0;
             for(int flag=0;flag<2;flag++){
                 if(flag==0)
-                    m = Math.max(dp[i+1][1]-arr[i],dp[i+1][0]);
+                    m = Math.max(next[1]-arr[i],next[0]);
                 else
-                    m = Math.max(dp[i+2][0]+arr[i],dp[i+1][1]);
-                dp[i][flag] = m;
+                    m = Math.max(nextnext[0]+arr[i],next[1]);
+                cur[flag] = m;
             }
+            System.arraycopy(next, 0, nextnext, 0, 2);
+            System.arraycopy(cur, 0, next, 0, 2);   
         }
-        return dp[0][0];
+        return cur[0];
     }
 }
