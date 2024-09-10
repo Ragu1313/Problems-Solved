a5df3592449920160a5d65df169685afbc32113d
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-09-10 09:20:17 +0530

    [2807] [Time Beats: 37.94%] [Memory Beats: 21.51%] - LeetPush

---
+++ 2807-Insert-Greatest-Common-Divisors-in-Linked-List.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int gcd(int n1,int n2){
+        int rem = n1%n2;
+        while(rem!=0){
+            n1 = n2;
+            n2 = rem;
+            rem = n1%n2 ;
+        }
+        return n2;
+    }
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        System.out.println(gcd(18,6));
+        ListNode cur = head.next;
+        ListNode prev = head;
+        if(cur==null)
+        return head;
+        while(cur!=null){
+            ListNode temp1 = new ListNode(gcd(cur.val,prev.val));
+            temp1.next = prev.next;
+            prev.next = temp1;
+            prev = prev.next;
+            prev = prev.next;
+            cur = cur.next;
+        }
+        return head;
+    }
+}
