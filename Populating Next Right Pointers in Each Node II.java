bb380c3a39bc9f702509178c924aea247a43a5c4
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-09-25 12:01:31 +0530

    Create Populating Next Right Pointers in Each Node II.java

---
+++ Populating Next Right Pointers in Each Node II.java
@@ -0,0 +1,121 @@
+/*
+117. Populating Next Right Pointers in Each Node II
+Solved
+Medium
+Topics
+Companies
+Given a binary tree
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Example 2:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 6000].
+-100 <= Node.val <= 100
+ 
+
+Follow-up:
+
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public void bfs(Node root){
+        Queue<Node> q = new LinkedList<>();
+        if(root!=null)
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size()-1;
+            Node node = q.poll();
+            // System.out.println(node.val);
+            if(node.left!=null)
+            q.add(node.left);
+            if(node.right!=null)
+            q.add(node.right);
+            while(size-->0){
+                Node temp = q.poll();
+                // System.out.println(temp.val);
+                node.next = temp;
+                node = node.next;
+                if(node.left!=null)
+                q.add(node.left);
+                if(temp.right!=null)
+                q.add(temp.right);
+            }
+            node.next = null;    
+        }
+    }
+    public Node connect(Node root) {
+        if(root==null) return null;
+        bfs(root);
+        return root;
+    }
+}
