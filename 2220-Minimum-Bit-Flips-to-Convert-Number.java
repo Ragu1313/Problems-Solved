02b5d746f6b6f8c1c580dfc7bedb22494ee60e7e
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-03 22:20:05 +0530

    [2220] [Time Beats: 100.00%] [Memory Beats: 24.23%] - LeetPush

--- 2220-Minimum-Bit-Flips-to-Convert-Number.java
+++ 2220-Minimum-Bit-Flips-to-Convert-Number.java
@@ -1,12 +1,10 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        int res =0;
-        while(start!=goal){
-            int t1 = start&1;
-            int t2 = goal&1;
-            if(t1!=t2) res++;
-            start = start>>1;
-            goal = goal>>1;
+        int rem = (start^goal);
+        int res = 0;
+        while(rem!=0){
+            res = res+(rem&1);
+            rem = rem>>1;
         }
         return res;
     }
