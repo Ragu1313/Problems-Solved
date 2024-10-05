3b21cf08b6b2a3a8da7612d7d12c6de3b0aa5eb4
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-05 23:35:50 +0530

    [567] [Time Beats: 17.22%] [Memory Beats: 9.80%] - LeetPush

--- 567-Permutation-in-String.java
+++ 567-Permutation-in-String.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int arr[] = new int [140];
+        int arr[] = new int [26];
         int l = s1.length();
         for(int i=0;i<l;i++)
-            arr[s1.charAt(i)]++;
+            arr[s1.charAt(i)-'a']++;
         
         int len = 0;
         for(int i=0;i<=s2.length()-l;i++){
             len = 0;
-            int cnt[] = new int[140];
+            int cnt[] = new int[26];
             for(int j=i;j<l+i;j++){
                 char ch = s2.charAt(j);
                 // System.out.print(ch+\ \);
-                if(cnt[ch]<arr[ch] && arr[ch]>0){
+                if(cnt[ch-'a']<arr[ch-'a'] && arr[ch-'a']>0){
                     // System.out.println(ch);
                     len++;
-                    cnt[ch]++;
+                    cnt[ch-'a']++;
                 }
             }
             // System.out.println();
