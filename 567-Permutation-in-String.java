35c2df4f88f4dc397e807f5085bf145556a849a8
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-06 00:12:02 +0530

    [567] [Time Beats: 72.48%] [Memory Beats: 41.00%] - LeetPush

--- 567-Permutation-in-String.java
+++ 567-Permutation-in-String.java
@@ -1,27 +1,32 @@
 class Solution {
+    public boolean check(int arr[],int cnt[]){
+        for(int i=0;i<26;i++){
+            if(arr[i]!=cnt[i])  return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
         int arr[] = new int [26];
+        int cnt[] = new int[26];
         int l = s1.length();
         for(int i=0;i<l;i++)
             arr[s1.charAt(i)-'a']++;
-        
         int len = 0;
-        for(int i=0;i<=s2.length()-l;i++){
-            len = 0;
-            int cnt[] = new int[26];
-            for(int j=i;j<l+i;j++){
-                char ch = s2.charAt(j);
-                // System.out.print(ch+\ \);
-                if(cnt[ch-'a']<arr[ch-'a'] && arr[ch-'a']>0){
-                    // System.out.println(ch);
-                    len++;
-                    cnt[ch-'a']++;
-                }
-            }
-            // System.out.println();
-            if(len==l)  return true;
+        for(int i=0;i<l;i++){
+            char ch = s2.charAt(i);
+            cnt[ch-'a']++;
         }
-        return false;
-        
+        if(check(arr,cnt))
+            return true;
+        // int poin = 0;
+        for(int i=l;i<s2.length();i++){
+            char prev = s2.charAt(i-l);
+            cnt[prev-'a']--;
+            char ch = s2.charAt(i);
+            cnt[ch-'a']++;
+            if(check(arr,cnt))  return true;
+        }
+        return false;    
     }
 }
