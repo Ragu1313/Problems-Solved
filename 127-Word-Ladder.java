0510b7353da9d42181200b374f2ed98fa33a7e60
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-05 14:24:16 +0530

    [127] [Time Beats: 10.92%] [Memory Beats: 97.70%] - LeetPush

---
+++ 127-Word-Ladder.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean check(String start,String end){
+        int cnt=0;
+        for(int i=0;i<start.length();i++){
+            if(start.charAt(i)!=end.charAt(i)){
+                cnt++;
+            }
+        }
+        return cnt==1;
+    }
+    public int bfs(String start,String end,List<String> word){
+        if(start.equals(end))  return 0;
+        Queue<String> q = new LinkedList<>();
+        q.add(start);
+        Map<String,Integer> map = new HashMap<>();
+        for(String s:word)
+            map.put(s,0);
+        int cnt=0;
+        // if(map.containsKey(start)){map.put(start,1);}
+        // else    {
+            
+        //     cnt++;
+        // }
+        map.put(start,1);
+        cnt++;
+        if(!map.containsKey(end))   return 0;
+        while(!q.isEmpty()){
+            cnt++;
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                String temp = q.poll();
+                for(String s:word){
+                    if(check(s,temp) && map.get(s)==0){
+                        q.add(s);
+                        // System.out.println(s);
+                        if(s.equals(end))   return cnt;
+                        map.put(s,1);
+                    }
+                }
+            }
+            // System.out.println(cnt);
+        }
+        return 0;
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList){
+        return bfs(beginWord,endWord,wordList);
+    }
+}
