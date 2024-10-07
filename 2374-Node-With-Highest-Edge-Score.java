35073b73dc813c15a9cccf337ee27516dd0595dd
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-07 17:54:36 +0530

    [2374] [Time Beats: 27.15%] [Memory Beats: 6.33%] - LeetPush

--- 2374-Node-With-Highest-Edge-Score.java
+++ 2374-Node-With-Highest-Edge-Score.java
@@ -1,25 +1,25 @@
 class Solution {
     public int edgeScore(int[] arr) {
-        Map<Integer,Long> map = new HashMap<>();
-        // int res = 0
-        // Map<Integer,Integer> map2 = new HashMap<>();
+        // Map<Integer,Long> map = new HashMap<>();
+        long map[] = new long[arr.length];
         for(int i=0;i<arr.length;i++){
-            map.put(arr[i],Long.valueOf(0));
+            // map.put(arr[i],Long.valueOf(0));
+            map[arr[i]] = Long.valueOf(0);
         }
         for(int i=0;i<arr.length;i++){
             int temp = arr[i];
-            map.put(temp,Long.valueOf(map.get(temp)+i));
+            // map.put(temp,Long.valueOf(map.get(temp)+i));
+            map[temp] = Long.valueOf(map[temp]+i);
         }
         long res = -1;
         int index = -2;
         for(int set=0;set<arr.length;set++){
-            // int set = i;
-            if(map.containsKey(set)){
-                if(res<map.get(set)){
+            // if(map.containsKey(set)){
+                if(res<map[set]){
                     index = set;
-                    res = map.get(set);
+                    res = map[set];
                 }
-            }
+            // }
         }
         return index;
     }
