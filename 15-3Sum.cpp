b64f183b5f3641f9505c3e1f9f8a4fd2a7ea6c30
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-14 23:44:53 +0530

    [15] [Time Beats: 6.98%] [Memory Beats: 17.78%] - LeetPush

---
+++ 15-3Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+    
+        sort(nums.begin(),nums.end());
+        set<vector<int>> s ;
+        for(int i=0;i<nums.size();i++){
+            int j = i+1;
+            int r = nums.size()-1;
+            while(j<r){
+                int temp = nums[i]+nums[j]+nums[r];
+                if(temp == 0){
+                    s.insert(vector<int>{nums[i],nums[j],nums[r]});
+                    j++;
+                    r--;
+                }
+                else if(temp<0) j++;
+                else    r--;
+            }
+        }
+        vector<vector<int>> res(s.begin(),s.end());
+        return res;
+    }
+};
