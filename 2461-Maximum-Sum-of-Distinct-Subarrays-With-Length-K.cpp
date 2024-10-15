199600ad8961a2aec20aaa3399892b20284d9115
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-15 22:48:15 +0530

    [2461] [Time Beats: 16.00%] [Memory Beats: 18.81%] - LeetPush

---
+++ 2461-Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        map<int,int> m;
+        long long res  = 0;
+        long long sum = 0;
+        for(int i=0;i<k;i++){
+            m[nums[i]]++;
+            sum = sum+nums[i];
+        }
+        if(m.size()==k){
+            res = sum;
+        }
+        for(int i=k;i<nums.size();i++){
+            m[nums[i]]++;
+            m[nums[i-k]]--;
+            sum = sum-nums[i-k]+nums[i];
+            if(m[nums[i-k]]==0){
+                m.erase(nums[i-k]);
+            }
+            if(m.size()==k){
+                res = max(res,sum);
+            }
+        }
+        return res;
+
+    }
+};
