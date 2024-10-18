4b127d4236758b6bdbbba61cd232af19d6310337
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-18 19:40:28 +0530

    [278] [Time Beats: 5.98%] [Memory Beats: 19.24%] - LeetPush

---
+++ 278-First-Bad-Version.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        // boolean res = false;
+        int l = 1;
+        int r = n;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            System.out.println(mid);
+            boolean res = isBadVersion(mid);
+            boolean res1 = true;
+            if(mid!=0)
+            res1 = isBadVersion(mid-1);
+            if(res && !res1){
+                return mid;
+            }
+            else if(res && res1) r=mid-1;
+            else if(!res && !res)   l = mid+1;
+        }
+        return -1;
+    }
+}
