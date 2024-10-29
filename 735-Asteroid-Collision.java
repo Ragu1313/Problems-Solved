fb0d7f84d8c71b4f31679cb94e44844b8eab5384
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-10-29 21:53:03 +0530

    [735] [Time Beats: 87.81%] [Memory Beats: 55.35%] - LeetPush

--- 735-Asteroid-Collision.java
+++ 735-Asteroid-Collision.java
@@ -1,59 +1,48 @@
 class Solution {
     public int[] asteroidCollision(int[] arr) {
-        Stack<Integer> a = new Stack<>();
-        a.push(arr[0]);
-        for(int i=1;i<arr.length;i++){
-            int t = arr[i];
-            int temp1 = t;
-            int temp2 ;
-            // try{
-            if(!a.isEmpty())
-                temp2= a.peek();
-            else{
-                a.push(temp1);
-                continue;
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            if(stack.isEmpty()){
+                stack.push(arr[i]);
             }
-            if(!a.isEmpty()){
-                while(!a.isEmpty()){
-                    if((temp1>=0 && temp2>=0) || (temp1<0 && temp2<0) || (temp2<0 && temp1>0)){
-                        a.push(temp1);
-                        break;
-                    }
-                    else if(temp1+temp2==0){
-                        a.pop();
-                        break;
-                    }
-                    else{
-                            int res = temp1+temp2;
-                            if(temp2<0 && temp1>=0){
-                                a.push(temp1);
+            else{
+                int prev = stack.peek();
+                if(arr[i]<0 && prev>=0){
+                    // int temp = -arr[i];
+                    // int flag = 0;
+                    while(!stack.isEmpty()){
+                        prev = stack.peek();
+                        if(prev+arr[i]<=0  && prev>=0){
+                            stack.pop();
+                            if(prev+arr[i]==0)
+                                break;
+                            if(stack.isEmpty()){
+                                stack.push(arr[i]);
                                 break;
                             }
-                            if(res>0)
-                                break;
-                            // else if(res==0){
-                            //     a.pop();
-                            //     break;
-                            // }
-                            else if(res<0){
-                                a.pop();
-                                if(!a.isEmpty())
-                                    temp2 = a.peek();  
-                                else{
-                                    a.push(temp1);
-                                    break;
-                                }
-                                     
-                            }
+                            // flag = 1;
+                            // stack.push(arr[i]);
+                        }
+                        else if (prev<0){
+                            // flag = 0;
+                            stack.push(arr[i]);
+                            break;
+                        }
+                        else{
+                            // flag = 0;
+                            break;
+                        }
                     }
                 }
+                else{
+                    stack.push(arr[i]);
+                }
             }
-            else
-                a.push(t);    
         }
-        int [] ar = new int[a.size()];
-        for(int i=a.size()-1;i>=0;i--)
-            ar[i] = a.pop();
-        return ar;
+        int res[] = new int[stack.size()];
+        for(int i=stack.size()-1;i>=0;i--){
+            res[i] = stack.pop();
+        }
+        return res;
     }
 }
