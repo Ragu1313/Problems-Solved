93c86737c1d7036f43e5604c36fdd9a264ff8fd1
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-11-01 21:24:35 +0530

    [152] [Time Beats: 100.00%] [Memory Beats: 84.92%] - LeetPush

---
+++ 152-Maximum-Product-Subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int front = 1;
+        int back = 1;
+        int maxi = nums[0];
+        for(int i=0;i<nums.length;i++){
+            front = front*nums[i];
+            maxi = Math.max(maxi,front);
+            if(front==0)    front=1;
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            back = back*nums[i];
+            maxi = Math.max(maxi,back);
+            if(back==0) back=1;
+        }
+        for(int i=0;i<nums.length;i++){
+            // front = front*nums[i];
+            maxi = Math.max(maxi,nums[i]);
+        }
+        return maxi;
+
+    }
+}
