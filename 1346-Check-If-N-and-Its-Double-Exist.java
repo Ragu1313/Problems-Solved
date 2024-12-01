854505cfb8cb0fe91b7f06f2d14d76e6bbfad13e
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-12-01 09:09:05 +0530

    [1346] [Time Beats: 14.78%] [Memory Beats: 8.11%] - LeetPush

---
+++ 1346-Check-If-N-and-Its-Double-Exist.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        Set<Integer> map = new HashSet<>();
+        for(int i : arr){
+            // if(!((i&1)>0)){
+                if(map.contains(i<<1) ){
+                    return true;
+                }
+                else if(!((i&1)>0) && map.contains(i>>1)){
+                    return true;
+                }
+            // }
+            map.add(i);
+        }
+        return false;
+    }
+}
