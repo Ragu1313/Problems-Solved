3bdead395cf6012cae3727bcf4e8b5e29f7a3596
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-12-11 18:01:30 +0530

    [84] [Time Beats: 55.11%] [Memory Beats: 88.76%] - LeetPush

--- 84-Largest-Rectangle-in-Histogram.java
+++ 84-Largest-Rectangle-in-Histogram.java
@@ -13,20 +13,32 @@ class Solution {
             while(!s1.isEmpty() && hei[s1.peek()] >hei[i]){
                 int peek = s1.pop();
                 ns[peek] = i;
-                // s1.pop();
+                if(!s1.isEmpty()){
+                    ps[peek] = s1.peek();
+                }
             }
+            
             s1.push(i);
         }
-
-        for(int i=n-1;i>=0;i--){
-            while(!s2.isEmpty() && hei[s2.peek()]>hei[i]){
-                int peek = s2.pop();
-                ps[peek] = i;
+        while(!s1.isEmpty()){
+            int peek = s1.pop();
+            if(!s1.isEmpty()){
+                int next = s1.peek();
+                ps[peek] = next;
             }
-            s2.push(i);
+           
         }
+
+        // for(int i=n-1;i>=0;i--){
+        //     while(!s2.isEmpty() && hei[s2.peek()]>hei[i]){
+        //         int peek = s2.pop();
+        //         ps[peek] = i;
+        //     }
+        //     s2.push(i);
+        // }
         int res = 0;
         for(int i=0;i<n;i++){
+            // System.out.println(ns[i] + \ \+ps[i]);
             res = Math.max(res,hei[i]*(ns[i]-ps[i]-1));
         }
         return res;
