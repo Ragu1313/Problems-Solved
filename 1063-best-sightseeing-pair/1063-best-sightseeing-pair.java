601483dc2412a64f386ddd325e46138deaecaabb
RAGU <165628439+Ragu1313@users.noreply.github.com>
2024-12-27 22:18:04 +0530

    Time: 4 ms (89.46%), Space: 53.4 MB (61.35%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int f(int i,int flag ,int arr[],int dp[][]){
-        if(i<0 || flag==2){
-            return 0;
-        }
-        if(dp[i][flag]!=-1){
-            return dp[i][flag];
-        }
-        int pick = Integer.MIN_VALUE;
-        int notpick ;
-        if(flag==0)
-            pick = arr[i] -i+ f(i-1,flag+1,arr,dp);
-        else{
-            pick = arr[i]+ i+f(i-1,flag+1,arr,dp);
-        }
-        notpick = f(i-1,flag,arr,dp);
-
-        return dp[i][flag] =  Math.max(pick,notpick);
-    }
+    // public int f(int i,int flag ,int arr[],int dp[][]){
+    //     if(i<0 || flag==2){
+    //         return 0;
+    //     }
+    //     if(dp[i][flag]!=-1){
+    //         return dp[i][flag];
+    //     }
+    //     int pick = Integer.MIN_VALUE;
+    //     int notpick ;
+    //     if(flag==0)
+    //         pick = arr[i] -i+ f(i-1,flag+1,arr,dp);
+    //     else{
+    //         pick = arr[i]+ i+f(i-1,flag+1,arr,dp);
+    //     }
+    //     notpick = f(i-1,flag,arr,dp);
+    //     return dp[i][flag] =  Math.max(pick,notpick);
+    // }
     public int maxScoreSightseeingPair(int[] values) {
+        int maxi = Integer.MIN_VALUE;
         int n = values.length;
-        int [][] dp = new int[n][2];
-        // for(int i[][]:dp){
-            for(int j[]:dp){
-                Arrays.fill(j,-1);
-            }
-        // }
-        return f(n-1,0,values,dp);
+        int left = values[0];
+        for(int i=1;i<n;i++){
+            maxi = Math.max(maxi,left+values[i]-i);
+            left = Math.max(left,values[i]+i);
+        }
+        return maxi;
     }
 }
