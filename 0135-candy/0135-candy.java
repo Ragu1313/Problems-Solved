26fa079cc5df367f7fa51d1fe296c3657e0bff5b
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-01-09 14:40:15 +0530

    Time: 3 ms (83.58%), Space: 46 MB (27.54%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,31 +1,30 @@
 class Solution {
     public int candy(int[] arr) {
-        int res = 0;
+        int res = 1;
         int n = arr.length;
-        // int arr1[] = new int[n];
-        int arr2[] = new int[n];
-        // arr1[0] = 1;
-        arr2[n-1] = 1;
-        for(int i=n-2;i>=0;i--){
-            if(arr[i]>arr[i+1]){
-                arr2[i] = arr2[i+1]+1;
+        int l = 1;
+        // int pcost = 1;
+        while(l<n){
+            // pcost = 1;
+            if( arr[l]==arr[l-1]){
+                l++;
+                res++;
+                continue;
             }
-            else{
-                arr2[i] = 1;
+            int pcost = 1;
+            while(l < n && arr[l]>arr[l-1]){
+                pcost++;
+                res+=pcost;
+                l++;
             }
-        }
-        int prev = 1;
-        // int ele = arr[0];
-        res += Math.max(prev,arr2[0]);
-        for(int i=1;i<n;i++){
-            if(arr[i]>arr[i-1]){
-                prev = prev+1;
+            int small = 1;
+            while(l < n && arr[l]<arr[l-1]){
+                res+=small;
+                small++;
+                l++;
             }
-            else{
-                prev = 1;
-            }
-            res+=Math.max(prev,arr2[i]);
-            
+            if(small>pcost)
+            res = res+small-pcost;   
         }
         return res;
     }
