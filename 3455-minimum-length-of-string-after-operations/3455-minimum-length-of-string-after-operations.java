6b455b090229138f2cc8cacc2d55c0d621fe54ac
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-01-13 22:13:14 +0530

    Time: 27 ms (53.64%), Space: 46.1 MB (85.43%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -2,10 +2,10 @@ class Solution {
     public int minimumLength(String s) {
         int [] arr = new int[26];
         int l=0;
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            if(arr[s.charAt(i)-'a']>2){
-                arr[s.charAt(i)-'a'] = 1;
+        for(char ch : s.toCharArray()){
+            arr[ch-'a']++;
+            if(arr[ch-'a']>2){
+                arr[ch-'a'] = 1;
             }
         }
         for(int i : arr){
