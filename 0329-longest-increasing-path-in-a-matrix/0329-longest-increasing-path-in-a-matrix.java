654b1e918d13ee9942021b68df4aded11cf2d5c4
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-02-04 05:23:06 +0530

    Time: 7 ms (96.82%), Space: 45 MB (66.06%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int f(int i,int j,int mat[][],int vis[][],int n,int m,int dp[][]){
+    public int f(int i,int j,int mat[][],int n,int m,int dp[][]){
         // if(i<0 || i>=n || j<0 || j>=m || vis[i][j]==1){
         //     return 0;
         // }
-        if(dp[i][j]!=1 ){
+        if(dp[i][j]!=0 ){
             return dp[i][j];
         }
-        if(vis[i][j]==1 && dp[i][j]==1){
-            return 1;
-        }
-        vis[i][j] = 1;
+        // if(vis[i][j]==1 && dp[i][j]==1){
+        //     return 1;
+        // }
+        // vis[i][j] = 1;
         int left = 0;
         int right = 0;
         int top = 0;
         int bottom = 0;
         if(i+1>=0 && i+1<n &&  mat[i+1][j]>mat[i][j]){
-            top = 1+f(i+1,j,mat,vis,n,m,dp);
+            top = 1+f(i+1,j,mat,n,m,dp);
         }
         if(i-1>=0 && i-1<n &&  mat[i-1][j]>mat[i][j]){
-            bottom = 1+f(i-1,j,mat,vis,n,m,dp);
+            bottom = 1+f(i-1,j,mat,n,m,dp);
         }
         if(j+1>=0 && j+1<m &&  mat[i][j+1]>mat[i][j]){
-            right = 1+f(i,j+1,mat,vis,n,m,dp);
+            right = 1+f(i,j+1,mat,n,m,dp);
         }
         if(j-1>=0 && j-1<m &&  mat[i][j-1]>mat[i][j]){
-            left = 1+f(i,j-1,mat,vis,n,m,dp);
+            left = 1+f(i,j-1,mat,n,m,dp);
         }
 
         return dp[i][j] = Math.max(left,Math.max(right,Math.max(top,Math.max(1,bottom))));
@@ -33,11 +33,11 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         int grid[][] = new int[n][m];
-        for(int i[] : grid){
-            Arrays.fill(i,1);
-        }
+        // for(int i[] : grid){
+        //     Arrays.fill(i,1);
+        // }
         // for(int i=0)
-        int vis[][] = new int[n][m];
+        // int vis[][] = new int[n][m];
         int res = 1;
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<m;j++){
@@ -47,9 +47,9 @@ class Solution {
         // }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0){
-                    res = Math.max(res,f(i,j,matrix,vis,n,m,grid));
-                }
+                // if(vis[i][j]==0){
+                    res = Math.max(res,f(i,j,matrix,n,m,grid));
+                // }
             }
         }
         // for(int i=0;i<n;i++){
