58f0396d89baedd8a5c9a8df5ba9acc436ea205b
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-02-04 14:50:31 +0530

    Time: 2703 ms (5.03%), Space: 655.1 MB (5.13%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -1,44 +1,43 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
+// #include<bit/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    int f(vector<vector<int>>& adj, vector<int> wei, int n) { // Copy weight, pass adj by reference
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> p;
-        p.push({0, 0});
+    int f(vector<vector<int>> adj,vector<int> wei,int n){
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>> > p;
+        p.push({0,0});
         wei[0] = 0;
-
-        while (!p.empty()) {
+        while(p.size()>0){
             vector<int> temp = p.top();
             p.pop();
-            if (temp[1] == n - 1)
+            if(temp[1]==n-1)
                 return temp[0];
-
-            int val = temp[0] + 1;
-            for (auto i : adj[temp[1]]) {
-                if (wei[i] > val) {
+            int val = temp[0]+1;
+            for(auto i : adj[temp[1]]){
+                if(wei[i]>val){
+                    // cout<<i<<" ";
                     wei[i] = val;
-                    p.push({val, i});
-                }
+                    p.push({val,i});
+                }  
             }
         }
-        return wei[n - 1];
-    }
-
+        // int t = -2;
+        return wei[n-1];
+    };
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> adj(n);
-        vector<int> weight(n, INT_MAX);
+        vector<vector<int>> adj (n) ;
+        vector<int> weight (n,INT_MAX);
 
-        for (int i = 0; i < n - 1; i++) {
-            adj[i].push_back(i + 1);
+        for(int i=0;i<n-1;i++){
+            adj[i].push_back(i+1);
         }
-
+        
         vector<int> res;
-        for (auto& q : queries) {
-            int src = q[0], des = q[1];
+        for(auto i : queries){
+            int src = i[0];
+            int des = i[1];
             adj[src].push_back(des);
             
-            res.push_back(f(adj, weight, n));
+            res.push_back(f(adj,weight,n));
         }
         return res;
     }
