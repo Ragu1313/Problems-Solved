2fe9e0024c7b6c60478cc7ed8abee8f905735416
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-02-06 21:53:36 +0530

    Time: 919 ms (6.58%), Space: 99.4 MB (10.34%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,24 @@
+// #define ll long long ;
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        int res = 0;
+        map<int,int> m;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            
+            for(int j=i+1;j<n;j++){
+                int temp = nums[i]*nums[j];
+                m[temp]++;
+            }
+        }
+        // int res = 0;
+        for(auto& [k,v] : m){
+            if(v>1){
+                int temp = 4*(v)*(v-1);
+                res = res + temp;
+            }
+        }
+        return res;
+    }
+};
