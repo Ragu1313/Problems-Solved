9a52ae82c8e3f5e90974f3b56f8a938dc2bd2546
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-02-09 15:02:19 +0530

    Time: 138 ms (8.51%), Space: 89.4 MB (39.21%) - LeetHub

---
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) {
+        long long res = 0;
+        long long  n = nums.size();
+        map<long long ,int> s;
+        for(int i=0;i<n;i++){
+            // s.insert(nums[i]-i);
+            res += s[nums[i]-i];
+            s[nums[i]-i]++;
+            
+        }
+        return n*(n-1)/2 - res;
+    }
+};
