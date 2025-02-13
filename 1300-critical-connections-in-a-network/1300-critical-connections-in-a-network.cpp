0b7d5660ed00e8be700cd7d2c70a8d14604cb302
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-02-13 14:54:37 +0530

    Time: 191 ms (69.88%), Space: 184 MB (30.95%) - LeetHub

---
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int timer = 1;
+    void f(int v,int parent,vector<int>& vis,vector<int>& tim,vector<int>& low,vector<vector<int>>& res,vector<vector<int>>& adj){
+        vis[v] = 1;
+        tim[v] = low[v] =  timer;timer++;
+        for(auto i : adj[v]){
+            if(i==parent)   continue;
+            if(vis[i]==0){
+                vis[i] = 1 ;
+                f(i,v,vis,tim,low,res,adj);
+                low[v] = min(low[i],low[v]);
+                if(low[i]>tim[v]){
+                    res.push_back({i,v});
+                } 
+            }
+            else {
+                low[v] = min(low[i],low[v]);
+            }
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        for(auto i : connections){
+            adj[i[1]].push_back(i[0]);
+            adj[i[0]].push_back(i[1]);
+        }
+        vector<int> vis(n);
+        vector<int> tim(n);
+        vector<int> low(n);
+        vector<vector<int>> res;
+        f(0,-1,vis,tim,low,res,adj);
+        return res;
+    }
+};
