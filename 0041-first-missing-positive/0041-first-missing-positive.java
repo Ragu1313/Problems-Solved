c1795bd6a2ba8637c66e63b0b4c981d0afde651f
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-03-08 09:28:25 +0530

    Time: 654 ms (5.9%), Space: 56.2 MB (95.54%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,18 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        Set<Integer> set = new TreeSet<>();
+        // Set<Integer> set = new TreeSet<>();
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                set.add(nums[i]);
-            }
+            System.out.print(nums[i]+" ");
         }
+        int  j = 0;
+        while(j<n && nums[j]<=0)    j++;
         int num = 1;
-        for(int i : set){
+        while(j<n){
+            int i = nums[j];
             if(num!=i){
                 return num;
             }
             num++;
+            while(j<n && nums[j]==i)    j++;
+            // j++;
         }
 
         return num;
