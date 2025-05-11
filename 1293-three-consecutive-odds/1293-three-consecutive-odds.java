6662bb88acb4e4ddc27d1bce603aa2a9c002b15d
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-05-11 07:11:46 +0530

    Time: 0 ms (100%), Space: 42.9 MB (7.05%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int cnt = 0;
+        for(int i : arr){
+            if((i&1)==1){
+                cnt++;
+                if(cnt==3)  return true;
+            }
+            else    cnt = 0;
+        }
+        return false;
+    }
+}
