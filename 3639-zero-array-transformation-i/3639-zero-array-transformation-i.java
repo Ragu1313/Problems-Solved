e7d81c0ab0c5fb49ac722dc05157aec3904d1c5e
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-05-20 10:20:11 +0530

    Time: 2 ms (100%), Space: 95.9 MB (30.87%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int arr[] = new int[100002];
+        int n = nums.length;
+        int arr[] = new int[n+1];
         for(int i[] : queries){
             arr[i[0]]++;
             arr[i[1]+1]--;
         }
         int sum = 0;
-        for(int i=0;i<nums.length;i++){
+        
+        for(int i=0;i<n;i++){
             sum+=arr[i];
             if(nums[i]>sum) return false;
         }
