f7421487dab9cd85524820979a42249ada107133
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-06-15 22:50:36 +0530

    Time: 1 ms (92.38%), Space: 40.7 MB (59.05%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.java
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.java
@@ -2,24 +2,20 @@ class Solution {
     public int maxDiff(int num) {
         StringBuilder str = new StringBuilder(String.valueOf(num));
         StringBuilder str2 = new StringBuilder(str);
-        int cnt = 0;
+        // char cnt = '';
         int n = str.length();
-        int arr[] = new int[10];
+        // int arr[] = new int[10];
         for(int i=0;i<n;i++){
-            if(str.charAt(i)!='9' && arr[i]==0){
-                cnt++;
+            if(str.charAt(i)!='9' ){
+                // cnt++;
                 char ch = str.charAt(i);
                 for(int j=i;j<n;j++)
-                if(str.charAt(j)==ch){ str.setCharAt(j,'9');  arr[j] = 1;} 
+                if(str.charAt(j)==ch){ str.setCharAt(j,'9');  } 
                 break;
             }
         }
-        cnt = 0;
-        // if(n>2){
-        Arrays.fill(arr,0);
-        
         for(int i=0;i<n;i++){
-            
+            // cnt = str2.charAt(0);
             if(i==0 && str2.charAt(i)!='1'  ){
                 // cnt++;
                 char ch = str2.charAt(i);
@@ -27,26 +23,27 @@ class Solution {
                     if(str2.charAt(j)==ch){
                     // if(cnt==1)
                         str2.setCharAt(j,'1');
-                        arr[Integer.parseInt(""+str2.charAt(j))] = 1;
+                        // arr[Integer.parseInt(""+str2.charAt(j))] = 1;
                     }
                 }
+                // cnt = str2.charAt(i);
                 break;
                 // if(cnt ==2)  break;
             }
-            else if(i!=0 && str2.charAt(i)!='0' && arr[Integer.parseInt(str2.charAt(i)+"")]==0 ){
-                cnt++;
+            else if(i!=0 && str2.charAt(i)!='0'&& str2.charAt(i)!=str2.charAt(0) ){
+                // cnt++;
                 char ch = str2.charAt(i);
                 for(int j=i;j<n;j++){
                     if(str2.charAt(j)==ch  )
                     {
                         str2.setCharAt(j,'0');
-                        arr[Integer.parseInt(str2.charAt(j)+"")] = 1;
+                        // arr[Integer.parseInt(str2.charAt(j)+"")] = 1;
                     }
                 }
                 // str.setCharAt(i,'1');
                 break;
             }
-            arr[Integer.parseInt(str2.charAt(0)+"")] = 1;
+            // arr[] = 1;
         } 
         // System.out.println(str.toString()+" "+str2.toString());
         return Integer.parseInt(str.toString())-Integer.parseInt(str2.toString());
