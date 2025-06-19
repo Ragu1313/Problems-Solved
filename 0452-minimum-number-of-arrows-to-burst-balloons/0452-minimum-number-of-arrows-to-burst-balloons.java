d350f1e62a74eb2acfd80fca9241a9d188fbff32
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-06-19 21:41:36 +0530

    Time: 53 ms (65.73%), Space: 68.8 MB (89.32%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,33 +1,21 @@
 class Solution {
-    public int findMinArrowShots(int[][] p) {
-        Arrays.sort(p,(a,b)->{
-            int cmp = Integer.compare(a[0],b[0]);
-            if(cmp!=0)  return cmp;
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points,(a,b)->{
+            if(a[0]!=b[0]) return Integer.compare(a[0],b[0]);
             return Integer.compare(a[1],b[1]);
         });
-        int n = p.length;
-        // int res = 1;
-        int i=0;
-        int cnt = 0;
-        // for(i=0;i<n;i++){
-        //     System.out.println(p[i][0]+" "+p[i][1]);
-        // }
-        i=0;
-        while(i<n){
-            // int start = p[i][0];
-            int end = p[i][1];
-            int j = i+1;
-            while(j<n && end>=p[j][0]){
-                end = Math.min(end,p[j][1]);
-                j++;
-                // System.out.println("Hi   ");
+        int res = 0;
+        int n = points.length;
+        int prev = points[0][1];
+        for(int i=1;i<n;i++){
+            if(prev<points[i][0]){
+                res++;
+                prev = points[i][1];
             }
-            cnt++;
-
-            i = j;
-            // i++;
-            // System.out.println(i);
+            else{
+                prev = Math.min(prev,points[i][1]);
+            }
         }
-        return cnt;
+        return res+1;
     }
 }
