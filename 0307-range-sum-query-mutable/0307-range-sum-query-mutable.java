86ede9c59f48112cc34f50e293e39334dfd61dc0
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-07-25 22:13:33 +0530

    Time: 73 ms (80.91%), Space: 75.8 MB (66.78%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,54 +1,46 @@
 class NumArray {
 
-    int arr[] ;
-    int st[] ;
+    int nums[] ;
+    int fen[] ;
     public NumArray(int[] nums) {
-        arr = nums;
-        st = new int[4*nums.length];
-        build(0,0,nums.length-1);
+        this.nums = nums;
+        fen = new int[nums.length+1];
+        // fen = Arrays.copyOf(nums,nums.length);
+        for(int i=0;i<nums.length;i++){
+            add(i,nums[i]);
+        }
     }
 
-    public void build(int i,int l,int h){
-        if(l==h){
-            st[i] = arr[l];
-            return ;
+    public void add(int index,int val){
+        index++;
+        while(index<=nums.length){
+            fen[index]+=val;
+            index += index & -index;
         }
-        int mid = l+(h-l)/2;
-        build(2*i+1,l,mid);
-        build(2*i+2,mid+1,h);
-        st[i] = st[i*2+1]+st[i*2+2];
     }
     
     public void update(int index, int val) {
-        updateHelper(0, 0, arr.length - 1, index, val);
+        // index;
+        int dif = val - nums[index];
+        nums[index] = val;
+        add(index,dif);
     }
 
-    public void updateHelper(int i, int l, int h, int index, int val) {
-        if (l == h && l==index) {
-            st[i] = val;
-            return;
+    public int sum(int index){
+        int sum = 0;
+        index++;
+        while(index>0){
+            sum+=fen[index];
+            index -= index & -index;
         }
-        int mid = l + (h - l) / 2;
-        if (index <= mid) {
-            updateHelper(2 * i + 1, l, mid, index, val);
-        } else {
-            updateHelper(2 * i + 2, mid + 1, h, index, val);
-        }
-        st[i] = st[2 * i + 1] + st[2 * i + 2];
+        return sum;
     }
     
     public int sumRange(int left, int right) {
-        return f(left,right,0,0,arr.length-1);
-    }
-
-    public int f(int l,int h,int i,int start,int end){
-        if(h<start || end<l)    return 0;
-        if(l<=start && h>=end)  return st[i];
-        int mid = start+(end-start)/2;
-        return f(l,h,i*2+1,start,mid) + f(l,h,i*2+2,mid+1,end);
+        
+        return sum(right) - sum(left-1); 
     }
 }
-
 
 /**
  * Your NumArray object will be instantiated and called as such:
