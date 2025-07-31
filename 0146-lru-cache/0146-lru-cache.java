abacc4bd1e7952315d8a9bc0ef5306244b05a7e5
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-07-31 21:06:38 +0530

    Time: 49 ms (38.93%), Space: 124.1 MB (16.23%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,64 +1,66 @@
 class Node{
-    // int key;
     int val;
     Node prev;
     Node next;
     int key;
     public Node(int key,int val){
         this.key = key;
+        this.val = val;
         prev = null;
         next = null;
-        this.val = val;
     }
 }
-
 class LRUCache {
-    // Deque<Map<Integer,Integer>> dp = new LinkedList<>();
+    int capacity ; 
+    Node head = new Node(-1,-1);
+    Node tail = new Node(-1,-1);
     Map<Integer,Node> map = new HashMap<>();
-    Node head = new Node(0,0);
-    Node tail = new Node(0,0);
-    int cap ;
-    public void insertFirst(int key,Node newNode){
-        map.put(key,newNode);
-        Node temp = head.next;
-        temp.prev = newNode;
-        newNode.next = temp;
-        newNode.prev = head;
-        head.next = newNode;
-    }
-    public void delete(Node node){
-        int key = node.key;
-        map.remove(key);
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-    
-    // int capacity; 
     public LRUCache(int capacity) {
-        // this.capacity = capacity;
-        cap = capacity;
+        this.capacity = capacity ;
         head.next = tail;
         tail.prev = head;
     }
     
+    public void insertFirst(Node newNode){
+        newNode.next = head.next;
+        newNode.prev = head;
+        head.next.prev = newNode;
+        head.next = newNode;
+        map.put(newNode.key , newNode);
+        if(map.size()==capacity+1)  removeLast();
+    }
+
+    public void removeLast(){
+        Node prev = tail.prev;
+        prev.prev.next = tail;
+        tail.prev = prev.prev;
+        map.remove(prev.key);
+    }
+
+    public void f(Node node){
+        Node prev = node.prev;
+        Node next = node.next;
+        prev.next = next;
+        next.prev = prev;
+        insertFirst(node);
+    }
+
     public int get(int key) {
-        if(map.get(key)==null){
-            return -1;
+        if(map.get(key)!=null)  {
+            f(map.get(key));
+            return map.get(key).val;
         }
-        Node temp = map.get(key);
-        int val = map.get(key).val;
-        delete(map.get(key));
-        insertFirst(key,new Node(key,val));
-        return val;
+        return -1;
     }
     
-    public void put(int key, int val) {
-        if(map.get(key)!=null){
-            delete(map.get(key));
+    public void put(int key, int value) {
+        if(map.get(key)!=null) { 
+            map.get(key).val = value;
+            f(map.get(key)) ;
         }
-        if(cap==map.size())
-        delete(tail.prev);
-        insertFirst(key,new Node(key,val));
+        else
+        insertFirst(new Node(key,value));
+        return ;
     }
 }
 
