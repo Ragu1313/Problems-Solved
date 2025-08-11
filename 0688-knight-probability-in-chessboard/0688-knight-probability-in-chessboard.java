a99cea2ef274266906f67d66f551584486d7c61d
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-08-11 18:32:14 +0530

    Time: 5 ms (88.75%), Space: 42.3 MB (73.47%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.java
@@ -1,33 +1,26 @@
 class Solution {
-    // double cnt = 0;
-    public double f(int row,int col,int k,int n,double dp[][][]){
-        if(row<0 || row>=n || col>=n || col<0 ) return 0;
-        if(k==0){
-            // cnt++;
-           return 1.0;
+    int dir[][] = {
+        {-2,-1},{-1,-2},{1,-2},{2,-1},{2,1},{1,2},{-1,2},{-2,1}
+    };
+    public double f(int k,int row,int col,int n,double dp[][][]){
+        if(row<0 || col<0 || row>=n || col>=n)  return 0.0;
+        if(k==0) {
+            return 1;
         }
-        if(dp[row][col][k]!=-1) return dp[row][col][k];
-        double m1 = f(row+1,col+2,k-1,n,dp)*0.125;
-        double m2 = f(row+2,col+1,k-1,n,dp)*0.125;
-        double m3 = f(row+1,col-2,k-1,n,dp)*0.125;
-        double m4 = f(row+2,col-1,k-1,n,dp)*0.125;
-
-        double m5 = f(row-1,col-2,k-1,n,dp)*0.125;
-        double m6 = f(row-2,col-1,k-1,n,dp)*0.125;
-        double m7 = f(row-1,col+2,k-1,n,dp)*0.125;
-        double m8 = f(row-2,col+1,k-1,n,dp)*0.125;
-
-        dp[row][col][k] = (m1+m2+m3+m4+m5+m6+m7+m8);
-        return dp[row][col][k];
+        if(dp[k][row][col]!=-1)  return dp[k][row][col];
+        double res = 0.0;
+        for(int i=0;i<8;i++){
+            res += (f(k-1,row+dir[i][0],col+dir[i][1],n,dp)*0.125);
+        }
+        return dp[k][row][col] = res;
     }
     public double knightProbability(int n, int k, int row, int col) {
-        double dp[][][] = new double[101][101][k+1];
-        for(double i[][]:dp){
-            for(double j[]:i)
-            Arrays.fill(j,-1);
+        double dp[][][] = new double[k+1][n][n];
+        for(double i[][] : dp){
+            for(double j[] : i){
+                Arrays.fill(j,-1);
+            }
         }
-        double res = f(row,col,k,n,dp);
-        // System.out.println(res+" "+res/cnt);
-        return res;
+        return f(k,row,col,n,dp);
     }
 }
