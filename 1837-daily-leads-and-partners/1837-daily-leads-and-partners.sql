efb8b36311b0aed078fef869713cfe698ad4261f
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-08-19 23:28:18 +0530

    Time: 540 ms (50.71%), Space: 0B (100%) - LeetHub

---
+++ 1837-daily-leads-and-partners/1837-daily-leads-and-partners.sql
@@ -0,0 +1,9 @@
+
+
+    select date_id , make_name ,
+    Count(Distinct(lead_id)) as unique_leads ,
+    Count(Distinct(partner_id)) as unique_partners
+    from DailySales 
+    group by date_id,make_name
+
+
