bad5d4c88b0ad963045d265b56862c113cc0272d
RAGU <165628439+Ragu1313@users.noreply.github.com>
2025-10-02 11:08:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Unique K-Number Sum/unique-knumber-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    Set<ArrayList<Integer>> set ;
+    public void f(int ind,int target,int k,boolean vis[],ArrayList<Integer> arr){
+        
+        if(target==0 && k==0){
+            set.add(new ArrayList<>(arr));
+            return ;
+        }
+        
+        // if()
+        
+        if(target<0 || k<0)  return ;
+        
+        for(int i=ind+1;i<10;i++){
+            if(!vis[i] && target-i>=0){
+                vis[i] = true;
+                arr.add(i);
+                f(i,target-i,k-1,vis,arr);
+                arr.remove(arr.size()-1);
+                vis[i] = false;
+            }
+        }
+    }
+    
+    public ArrayList<ArrayList<Integer>> combinationSum(int n, int k) {
+        set = new HashSet<>();
+        f(0,n,k,new boolean[10],new ArrayList<>());
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        for(ArrayList<Integer> i : set){
+            res.add(i);
+        }
+        return res;
+    }
+}
